879434cd68bdf82a69d4bc1cc82c8f0668128440
Mahmmad khamar shaikh <shahid13384@hotmail.com>
2020-09-23 07:44:49 +0530

    GetAuthors method implemented

--- webapi/ZaynlibBookAPI/Controllers/AuthorsController.cs
+++ webapi/ZaynlibBookAPI/Controllers/AuthorsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Zaynlib.Data;
+using ZaynlibBookAPI.Services;
 
 namespace ZaynlibBookAPI.Controllers
 {
@@ -10,6 +13,27 @@ namespace ZaynlibBookAPI.Controllers
     [ApiController]
     public class AuthorsController : ControllerBase
     {
+        private readonly ZainlibBooksStoreContext _authorDbContext;
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
 
+        public AuthorsController(ZainlibBooksStoreContext context,
+            IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorDbContext = context ?? throw new ArgumentNullException(nameof(context));
+            _authorRepository = authorRepository ?? throw new ArgumentNullException(nameof(authorRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            var authorList = await _authorRepository.GetAuthorsAsync();
+            if (authorList == null)
+            {
+                return NotFound();
+            }
+            return Ok(authorList);
+        }
     }
 }
